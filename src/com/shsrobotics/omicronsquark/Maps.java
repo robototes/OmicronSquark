637234875f2606f9c5bf5230eb7aa6ad5177d5f3
Cory McCartan <cory.mccartan@gmail.com>
2012-12-17 20:41:46 -0800

    basic arcade drive
    arcade drive with joystick thresholds and cubed inputs

--- src/com/shsrobotics/omicronsquark/Maps.java
+++ src/com/shsrobotics/omicronsquark/Maps.java
@@ -6,10 +6,22 @@ public interface Maps {
       Joystick joystick = new Joystick(1);
 
       public static final class Robot {
-	    
+	    public static final int
+		    leftWheels = 1,
+		    rightWheels = 1;
+      }
+
+      public static final class Constants {
+	    public static final double
+		    driveCoordinateScale = 0.5,
+		    normalScale = 1.0,
+		    joystickThreshold = 0.1;
       }
 
       public static final class Buttons {
 
+	    // buttons that are only read for values
+	    public static final int
+		    scaleDriveCoordinates = 1;
       }
 }
--- src/com/shsrobotics/omicronsquark/subsystems/DriveTrain.java
+++ src/com/shsrobotics/omicronsquark/subsystems/DriveTrain.java
@@ -1,11 +1,25 @@
 
 package com.shsrobotics.omicronsquark.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import com.shsrobotics.omicronsquark.Maps;
 import com.shsrobotics.omicronsquark.commands.*;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.sun.squawk.util.MathUtils;
 
-public class DriveTrain extends Subsystem {
-      
+public class DriveTrain extends Subsystem implements Maps {
+
+      private RobotDrive robotDrive = new RobotDrive(Robot.leftWheels, Robot.rightWheels);
+      private double scalingFactor = MathUtils.pow(1 - Constants.joystickThreshold, -3);
+
+      public void drive(double forward, double turn) {
+	    forward = MathUtils.pow(forward, 3);
+		  forward = (forward > Constants.joystickThreshold) ? (forward - Constants.joystickThreshold) * scalingFactor : 0.0;
+	    turn = MathUtils.pow(turn, 3);
+		  turn = (turn > Constants.joystickThreshold) ? (turn - Constants.joystickThreshold) * scalingFactor : 0.0;
+	    robotDrive.arcadeDrive(forward, turn);
+      }
+
       public void initDefaultCommand() {
 	    setDefaultCommand(new DriveWithJoysticks());
       }
