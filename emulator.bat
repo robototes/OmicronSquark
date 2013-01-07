3cc8933b0e479cdb2cdaec8880cfd3ac3ddba925
Cory McCartan <cory.mccartan@gmail.com>
2013-01-06 20:12:51 -0800

    bugs in emulator.bat
    main > Main

--- emulator.bat
+++ emulator.bat
@@ -1,1 +1,3 @@
-@java -classpath ./suite/cRIO.jar;./suite/OmicronSquark_1.0.0.jar net.sourceforge.frcsimulator.Client --gui com.shsrobotics.omicronsquark.main
+@echo off
+java -classpath ./suite/cRIO.jar;./suite/OmicronSquark_1.0.0.jar net.sourceforge.frcsimulator.Client --gui com.shsrobotics.omicronsquark.Main
+pause
