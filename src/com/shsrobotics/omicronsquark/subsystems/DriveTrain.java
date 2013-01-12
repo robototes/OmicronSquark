f6f307e2946bd7b6bd7db069434056269ee96ad0
Max Notarangelo <maxnote1@gmail.com>
2013-01-12 15:35:24 -0800

    gyro
    turns out gyro class does what absolute gyroscope does

--- src/com/shsrobotics/omicronsquark/subsystems/DriveTrain.java
+++ src/com/shsrobotics/omicronsquark/subsystems/DriveTrain.java
@@ -2,18 +2,15 @@
 package com.shsrobotics.omicronsquark.subsystems;
 
 import com.shsrobotics.omicronsquark.Maps;
-import com.shsrobotics.omicronsquark.commands.*;
 import com.sun.squawk.util.MathUtils;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
-import com.shsrobotics.omicronsquark.commands.CommandBase;
 
 public class DriveTrain extends PIDSubsystem implements Maps {
 
 	private RobotDrive robotDrive = new RobotDrive(Robot.Drive.frontLeftWheel, Robot.Drive.rearLeftWheel, Robot.Drive.frontRightWheel, Robot.Drive.rearRightWheel);
-	private AbsoluteGyroscope gyroscope = new AbsoluteGyroscope(Robot.Drive.gyroscope);
+	private Gyro gyroscope = new Gyro(Robot.Drive.gyroscope);
 
 	private double scalingFactor = MathUtils.pow(1 - Constants.joystickThreshold, -3);
 	
@@ -31,7 +28,7 @@ public class DriveTrain extends PIDSubsystem implements Maps {
               x = (x > Constants.joystickThreshold) ? (x - Constants.joystickThreshold) * scalingFactor : 0.0;
             y = MathUtils.pow(y, 3);
               y = (y > Constants.joystickThreshold) ? (y - Constants.joystickThreshold) * scalingFactor : 0.0;
-            robotDrive.mecanumDrive_Cartesian(x, y, z, gyroscope.getAngularVelocity());
+            robotDrive.mecanumDrive_Cartesian(x, y, z, gyroscope.getAngle());
 	}
 
 	public double distanceLeft() {
