bd024a81342c7e0a26d736f7a71ba53f75f08666
Max Notarangelo <maxnote1@gmail.com>
2013-02-17 14:15:19 -0800

    fixed tabs and aspect ratios
    aspect ratios now account for retroreflective tape

--- src/com/shsrobotics/omicronsquark/Maps.java
+++ src/com/shsrobotics/omicronsquark/Maps.java
@@ -12,8 +12,8 @@ public interface Maps {
     Relay.Value ON = Relay.Value.kForward;
     Relay.Value OFF = Relay.Value.kOff;
 	
-	DoubleSolenoid.Value EXTENDED = DoubleSolenoid.Value.kForward;
-	DoubleSolenoid.Value RETRACTED = DoubleSolenoid.Value.kReverse;
+    DoubleSolenoid.Value EXTENDED = DoubleSolenoid.Value.kForward;
+    DoubleSolenoid.Value RETRACTED = DoubleSolenoid.Value.kReverse;
     
     public static final class Robot {
         public static final class Drive {
@@ -34,40 +34,40 @@ public interface Maps {
             public static final boolean
                 encoderReverseDirection = false;
         } 
-		public static final class Scorer {
+        public static final class Scorer {
             public static final int
-				encoderFrontA = 2,
-				encoderFrontB = 3,
-				encoderRearA = 4,
-				encoderRearB = 5,
+                encoderFrontA = 2,
+                encoderFrontB = 3,
+                encoderRearA = 4,
+                encoderRearB = 5,
                 flywheelFront = 5,
                 flywheelRear = 6,
                 loader = 1,
-				frisbeeCounterSwitch = 6,
-				loaderRegulatorSwitch = 7,
-				encoderPulsesPerRevolution = 256;
-			public static final double
-				P = 0.0,
-				I = 0.0,
-				D = 0.0;
+                frisbeeCounterSwitch = 6,
+                loaderRegulatorSwitch = 7,
+                encoderPulsesPerRevolution = 256;
+            public static final double
+                P = 0.0,
+                I = 0.0,
+                D = 0.0;
         }
-		public static final class Climber {
-			public static final int // ports				
+        public static final class Climber {
+            public static final int // ports				
                 pistonLifterA = 1,
-				pistonLifterB = 2,
+                pistonLifterB = 2,
                 limitSwitchUpperClimb = 8,
                 limitSwitchLowerClimb = 9,
-				leftTalon = 7,
-				rightTalon = 8;
-		}
-	}
+                leftTalon = 7,
+                rightTalon = 8;
+        }
+    }
     public static final class Constants {
         public static final double
             fieldLength = 54, // feet
             cameraHorizontalViewAngle = 47, // degrees
             cameraVerticalViewAngle = 36, // degrees
-			shooterAngleAdjustment = -12,
-			shooterVerticalAngle = 37,		// degrees
+            shooterAngleAdjustment = -12,
+            shooterVerticalAngle = 37,		// degrees
             significanceLevel_Angle = 3.0, //degrees
             significanceLevel_Rectangularity = 40, //percent            
             significanceLevel_Percent = 20, //percent            
@@ -76,15 +76,15 @@ public interface Maps {
             rotationStep = 15.0,
             spinRight = 1.0,
             spinLeft = -1.0,
-			frontToRearMotorSpeedRatio = 2000 / 5000, //rpm
-			shooterSpeedIncrement = 0.05,
-			rearMotorScaling = 0.8;
+            frontToRearMotorSpeedRatio = 2000 / 5000, //rpm
+            shooterSpeedIncrement = 0.05,
+            rearMotorScaling = 0.8;
         
         public static final class aspectRatios {
             public static final double
-                lowGoal = 29 / 24,
-                middleGoal = 54 / 21,
-                highGoal = 54 / 12;
+                lowGoal = (29 + 8) / (24 + 8),
+                middleGoal = (54 + 8) / (21 + 8),
+                highGoal = (54 + 8) / (12 + 8);
         }
         
         public static final int
@@ -93,25 +93,25 @@ public interface Maps {
             lowGoal = 1,
             middleGoal = 2,
             highGoal = 3,
-			groundLevel = 0,
-			firstLevel = 1,
-			secondLevel = 2,
-			thirdLevel = 3;
+            groundLevel = 0,
+            firstLevel = 1,
+            secondLevel = 2,
+            thirdLevel = 3;
     }
     
     public static final class Buttons {
         public static final JoystickButton 
             alignRobot = new JoystickButton(joystick, 7),
             zeroGyro = new JoystickButton(joystick, 11),
-			driveForwards = new JoystickButton(joystick, 2),
-			shoot = new JoystickButton(joystick, 1),
-			shooterUp = new JoystickButton(joystick, 6),
-			shooterDown = new JoystickButton(joystick, 4),
-			cancelLockToY = new JoystickButton(joystick, 12),
-			getRidOfOneFrisbee = new JoystickButton(joystick, 10);
-		
-		public static final DigitalIOButton
-			frisbeeCounter = new DigitalIOButton(Robot.Scorer.frisbeeCounterSwitch);		
+            driveForwards = new JoystickButton(joystick, 2),
+            shoot = new JoystickButton(joystick, 1),
+            shooterUp = new JoystickButton(joystick, 6),
+            shooterDown = new JoystickButton(joystick, 4),
+            cancelLockToY = new JoystickButton(joystick, 12),
+            getRidOfOneFrisbee = new JoystickButton(joystick, 10);
+
+        public static final DigitalIOButton
+            frisbeeCounter = new DigitalIOButton(Robot.Scorer.frisbeeCounterSwitch);		
         
         // buttons that are only read for values
         public static final int
