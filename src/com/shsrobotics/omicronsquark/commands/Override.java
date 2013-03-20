42345582e8a161acf98f6f5a1cf5c6171c8401b3
Max Notarangelo <maxnote1@gmail.com>
2013-03-20 16:43:42 -0700

    added override method
    currently does nothing

---
+++ src/com/shsrobotics/omicronsquark/commands/Override.java
@@ -0,0 +1,25 @@
+package com.shsrobotics.omicronsquark.commands;
+
+import com.shsrobotics.omicronsquark.Global;
+import com.shsrobotics.omicronsquark.Maps;
+
+public class Override extends CommandBase {
+	
+	public Override() {
+	}
+
+	protected void initialize() {
+		
+	}
+
+	protected void execute() {
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() { }
+
+	protected void interrupted() { }
+}
